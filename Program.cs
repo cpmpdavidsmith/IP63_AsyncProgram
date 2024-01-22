f4e5a8ec403d7eb3dbf7c1fe06f82605c1571a43
David Smith <cpmpdavidsmith@icloud.com>
2024-01-22 10:46:04 -0500

    second prac

--- Program.cs
+++ Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task task = new Task(ProcesstheDataAsync);
+            task.Start();
+            task.Wait();
+            Console.ReadLine();
+        }
+        static async void ProcesstheDataAsync()
+        {
+            Task<int> task = HandleFileAsync("c:\\enablel.txt");
+
+            Console.WriteLine("wait " + "while I carry out some vital. ");
+
+            int x = await task;
+            Console.WriteLine("count: " + x);
+        }
+        static async Task<int> HandleFileAsync(string file)
+        {
+            Console.WriteLine("HandleFile enter");
+            int count = 0;
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string v = await reader.ReadToEndAsync();
+
+                count += v.Length;
+                for(int i = 0; i < 10000; i++)
+                {
+                    int x = v.GetHashCode();
+                    if (x == 0)
+                    {
+                        count--;
+                    }
+                }
+            }
+            Console.WriteLine("HandleFile exit");
+            return count;
+         
+        }
+        
+    }
+    
+}
